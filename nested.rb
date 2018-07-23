266ba98a31966247920a11b44bd125b4d5a45198
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 15:47:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,11 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = {
-	 :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
+	programmer_hash = 
+	  {
+	    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
 	 },
         :grace_hopper => {
           :known_for => "COBOL",
