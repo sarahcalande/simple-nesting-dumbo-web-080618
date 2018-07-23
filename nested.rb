ebdf7039ce93412ee7e5110252bcfb4718e5b7d9
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 15:57:57 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -136,6 +136,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Smalltalk", "LISP", "Assembly"
- puts programmer_hash
+programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
+
 end
