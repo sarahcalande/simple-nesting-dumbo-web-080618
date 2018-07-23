209acabc3af94586a34787499f43e5297fba2b82
scalande@yahoo.com <scalande@yahoo.com>
2018-07-23 15:35:45 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -4,21 +4,21 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         }
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+#	programmer_hash = 
+ #		{
+  #      :grace_hopper => {
+   #       :known_for => "COBOL",
+    #      :languages => ["COBOL", "FORTRAN"]
+     #   },
+      #  :alan_kay => {
+       #   :known_for => "Object Orientation",
+        #  :languages => ["Smalltalk", "LISP"]
+        #},
+        #:dennis_ritchie => {
+         # :known_for => "Unix",
+          #:languages => ["C"]
+      #  }
+    # }
 
 
 end
